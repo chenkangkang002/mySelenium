11cc68b18e4dace50f9a0f7bf701b6ab926d1b66
411052590@qq.com <411052590@qq.com>
2020-07-20 09:48:56 +0800

    调整目录结构 将一些公共方法分类

---
+++ test001.py
@@ -0,0 +1,20 @@
+'''
+类作为参数
+'''
+# class book():
+#
+#     def print(self):
+#          print('我是book')
+#
+# class pen():
+#     def print(self):
+#         print('我是pen')
+#
+# def prints(cla):
+#     clas = cla()
+#     clas.print()
+#     return clas
+#
+# book = prints(book)
+# pen = prints(pen)
+# print(book, pen)
